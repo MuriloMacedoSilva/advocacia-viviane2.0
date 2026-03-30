69ec15adba528445dd0c59e11ec3f91b08793075
Murilo <murilo@192.168.15.46>
2026-03-30 11:09:26 -0300

    hhhh

--- src/components/sections/FourthSection.tsx
+++ src/components/sections/FourthSection.tsx
@@ -10,7 +10,7 @@ export default function FourthSection() {
         className="w-full relative object-cover"
       />
 
-      <div className="w-full relative z-10 h-87.5 flex items-center justify-start md:pl-[10%]">
+      <div className="w-full relative z-10 h-160 lg:h-87.5 flex items-center justify-start md:pl-[10%]">
         <h2 className="font-Montserrat text-white font-medium text-[27px] md:text-[36px] w-full lg:w-[60%] h-40 flex items-center justify-center flex-col border-l-5 pl-8 border-brand">
           "Não remediamos processos; Previnimos a falência da confiança."
         </h2>
