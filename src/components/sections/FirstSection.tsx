bc3eb306e38bc832ee1ff9ea3addf880a6192ce4
Murilo <murilo@192.168.15.46>
2026-03-30 20:12:28 -0300

    kkk

--- src/components/sections/FirstSection.tsx
+++ src/components/sections/FirstSection.tsx
@@ -16,7 +16,7 @@ export default function FirstSection() {
   }, []);
 
   return (
-    <section className="relative w-full h-screen xl:h-auto 2xl:h-[80vh] flex flex-col items-center pt-27  xl:pt-0 justify-center">
+    <section className="relative w-full h-screen xl:h-auto 2xl:h-[80vh] flex flex-col items-center  xl:pt-0 justify-center">
       <video
       ref={videoRef}
       poster="/background1.svg"
@@ -32,16 +32,16 @@ export default function FirstSection() {
 
       <div className="absolute inset-0 -z-1 bg-black/40"></div>
 
-      <div className="flex flex-col items-center xl:items-center justify-center w-[95%] xl:w-[71%] gap-6 xl:gap-10 2xl:gap-15 md:gap-12 mt-30 lg:mt-26 mb-50">
+      <div className="flex flex-col items-center xl:items-center justify-center w-[95%] xl:w-[71%] pt-29 xl:pt-0 gap-6 xl:gap-10 2xl:gap-15 md:gap-12 mt-30 lg:mt-26 mb-50">
         <div className="border border-brand p-1.5 rounded-full font-semibold max-w-125 xl:mb-6.5">
-          <p className="text-brand font-Montserrat xl:text-[12px] text-[8px] text-center">
+          <p className="text-brand font-Montserrat xl:text-[12px] text-[9px] text-center">
             BLINDAGEM JURÍDICA E COMPLIENCE ESTRATÉGICO
           </p>
         </div>
-        <h1 className="text-white font-Montserrat font-bold text-[40px] leading-12 xl:text-[64px] text-center xl:leading-18 mb-4 xl:mb-0">
+        <h1 className="text-white font-Montserrat font-bold text-[39px] leading-11 xl:text-[64px] text-center xl:leading-18 mb-4 xl:mb-0">
           A invisibilidade do risco é o maior passivo do seu negócio.
         </h1>
-        <p className="text-white font-poppins font-normal text-[12px] xl:text-[17px] text-center w-[90%] xl:w-[80%]">
+        <p className="text-white font-poppins font-normal text-[14px] xl:text-[17px] text-center w-[90%] xl:w-[80%]">
           Transformamos contingências legais e atritos de consumo em ativos de
           fidelização. Unimos o rigor do Compliance à inteligência de negócios
           para proteger sua margem e consolidar sua reputação global.
