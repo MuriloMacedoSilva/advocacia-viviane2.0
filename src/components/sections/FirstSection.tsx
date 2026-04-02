9cd1c04391dc6d7af6faaf6e2f63397dca50c24e
Murilo <murilo@192.168.15.46>
2026-04-01 23:50:35 -0300

    ggg

--- src/components/sections/FirstSection.tsx
+++ src/components/sections/FirstSection.tsx
@@ -38,14 +38,13 @@ export default function FirstSection() {
         playsInline
         className="absolute -z-2 min-w-full min-h-full object-cover"
       >
-        <source src="/backgroundBreak.webm" type="video/webm" />
-        <source src="/backgroundBreak.mp4" type="video/mp4" />
+        <source src="/backgroundBreake2.webm" type="video/webm" />
       </video>
 
       {/* Overlay escuro */}
       <div className="absolute inset-0 -z-1 bg-black/50" />
 
-      {/* ❌ REMOVA essa linha abaixo — ela causa o hydration error */}
+      {/* REMOVA essa linha abaixo — ela causa o hydration error */}
       {/* <div className="absolute inset-0 -z-3 bg-gray-950" /> */}
 
       {/* Container Principal */}
